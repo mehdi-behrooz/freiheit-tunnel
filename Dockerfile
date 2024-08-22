591b2af5bc9fa912c0467c8ad79df2c95806d4e6
Mehdi <mehdi.behrooz@gmail.com>
2024-08-22 20:40:15 +0000

    fix linting issues

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,11 @@
 # syntax=docker/dockerfile:1
+# checkov:skip=CKV_DOCKER_3: at least in server mode needs to be run as root in order to listen to different ports
+# checkov:skip=CKV_DOCKER_7: gost has no other stable release yet excpet "latest"
 
 FROM gogost/gost
 
-RUN apk update
-RUN apk add gettext         # for: envsubst
-
-# TODO: for debug purposes; remove them later
-RUN apk add moreutils       # for: sponge
-RUN apk add bash            # for: /bin/bash
-RUN apk add bind-tools      # for: dig
-RUN apk add nmap            # for: nmap
-RUN apk add curl            # for: curl
-RUN apk add vim             # for: vim
-RUN apk add python3         # for: python
+RUN apk update \
+    && apk add bash gettext-envsubst
 
 COPY ./conf /conf
 COPY --chmod=755 ./entrypoint.sh /
@@ -29,7 +22,6 @@ ENV LISTEN_ON=8443
 ENV SERVER_HOST=example.com
 ENV SERVER_PORT=8443
 ENV MAP=":80>:80;:443>:443"
-
 
 ENTRYPOINT ["/entrypoint.sh"]
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,32 +4,33 @@ case $MODE in
 
 server)
 
-    envsubst < /conf/server.yaml > /etc/gost.yaml
+    envsubst </conf/server.yaml.tmpl >/etc/gost.yaml
 
-;;
+    ;;
 
 client)
 
     services=""
     i=0
     for phrase in ${MAP//[;,]/ }; do
-        IFS=">" read -r remote_dest local_dest <<< $phrase
-        export number=$i remote_dest local_dest
-        service=`envsubst < /conf/client-service.yaml`
+        IFS=">" read -r remote_dest local_dest <<<"$phrase"
+        export number="$i" remote_dest local_dest
+        service=$(envsubst </conf/client-service.yaml.tmpl)
         services+="$service"$'\n'
         ((i++))
     done
     export services
-    envsubst < /conf/client.yaml > /etc/gost.yaml
+    envsubst </conf/client.yaml.tmpl >/etc/gost.yaml
 
-;;
+    ;;
+
 esac
 
 if [ "$LOG_LEVEL" == "debug" ]; then
 
     echo "Using config file: "
-    cat /etc/gost.yaml | sed 's/^/\t/'
+    sed 's/^/\t/' </etc/gost.yaml
 
 fi
 
-exec $@
+exec "$@"
